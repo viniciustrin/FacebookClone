6ab156248e48fe2444b1e28d197c5f4e1f1ace94
Vinicius Eduardo Trindade Antonio <vinicius.trindade@castinformatica.onmicrosoft.com>
2019-10-30 17:45:50 -0300

    Commit inicial

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
+
+        public ValuesController(ILogger<ValuesController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Value>> Get()
+        {
+            return await _context.Values.ToListAsync();
+        }
+    }
+}
